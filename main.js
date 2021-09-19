cc26fff07e5096a5cb6f27658a31daa0d72e0406
Chuck McCallum <mccalluc@users.noreply.github.com>
2021-09-19 18:04:12 -0400

    Mccalluc/no hard coding in js (#2)
    * pull into data attributes
    
    * math to the rescue

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
 function handleSuccess(position) {
-  const { coords } = position;
-  const { latitude, longitude, accuracy } = coords;
-  console.log(`lat: ${latitude} / long: ${longitude} / accuracy: ${accuracy}m`);
   document.getElementById('message').innerText = '';
 
-  var geoCoords = [
-    // lat, long
-    42.433977, -71.080992,
-    42.433977, -71.057395,
-    42.418675, -71.080992,
-    42.418675, -71.057395];
-  var imgCoords = [
-    // x, y; top left is (0, 0)
-    0, 0,
-    1242, 0,
-    0, 1052,
-    1242, 1052];
-  var perspT = PerspT(geoCoords, imgCoords);
+  const { coords } = position;
+  const { latitude, longitude } = coords;
+
   var geoPoint = [latitude, longitude];
-  var imgPoint = perspT.transform(geoPoint[0], geoPoint[1]);
+  var imgPoint = mapGeoToImg(...geoPoint);
+  console.log(`${geoPoint} -> ${imgPoint}`)
 
   const pin = document.getElementById('pin');
   pin.setAttribute('cx', imgPoint[0]);
@@ -29,6 +17,31 @@ function handleFailure(position) {
   document.getElementById('message').innerText = 'Geolocation API failed.';
 }
 
+export function mapGeoToImg(latitude, longitude) {
+  const mapData = document.getElementById('map').dataset;
+
+  const geoCoords = [
+    ...mapData.geo0.split(' '),
+    ...mapData.geo1.split(' '),
+    ...mapData.geo2.split(' '),
+    ...mapData.geo3.split(' ')];
+  const imgCoords = [
+    ...mapData.img0.split(' '),
+    ...mapData.img1.split(' '),
+    ...mapData.img2.split(' '),
+    ...mapData.img3.split(' ')];
+
+  // The differences between the geographic coordinates are very small when compared with their magnitudes.
+  // Translate the latitude and longitude to the origin before the projective transform.
+  const offset = geoCoords.slice(0, 2)
+  const offsetCoords = geoCoords.map((v, i) => v - offset[i % 2])
+
+  const perspT = PerspT(offsetCoords, imgCoords);
+  const geoPoint = [latitude - offset[0], longitude - offset[1]];
+  const imgPoint = perspT.transform(...geoPoint);
+  return imgPoint;
+}
+
 export default function main() {
-  navigator.geolocation.getCurrentPosition(handleSuccess, handleFailure);
+  navigator.geolocation.watchPosition(handleSuccess, handleFailure, { enableHighAccuracy: true });
 }
